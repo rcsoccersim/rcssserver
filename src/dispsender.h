42f16c72d56abf43dcf137ff2232314f76e47872
akiyama <akiyama@1639d2a8-b03b-4162-8b0b-03f2cde17881>
2008-02-11 02:55:20 +0000

    added new files.
    git-svn-id: file:///home/akiyama/sserver/svn/rcss/trunk/rcssserver@113 1639d2a8-b03b-4162-8b0b-03f2cde17881

---
+++ src/dispsender.h
@@ -0,0 +1,216 @@
+// -*-c++-*-
+
+/***************************************************************************
+                                dispsender.h
+                     Classes for building display messages
+                             -------------------
+    begin                : 2007-11-22
+    copyright            : (C) 2007 by The RoboCup Soccer Server
+                           Maintenance Group.
+    email                : sserver-admin@lists.sourceforge.net
+***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU LGPL as published by the Free Software  *
+ *   Foundation; either version 2 of the License, or (at your option) any  *
+ *   later version.                                                        *
+ *                                                                         *
+ ***************************************************************************/
+
+#ifndef RCSS_DISP_SENDER_H
+#define RCSS_DISP_SENDER_H
+
+#include "sender.h"
+#include "observer.h"
+
+#include <rcssbase/lib/factory.hpp>
+
+class Stadium;
+
+namespace rcss {
+class SerializerMonitor;
+
+/*!
+  \class DispSender
+  \brief Base class for the display protocol.
+*/
+class DispSender
+    : protected Sender
+{
+public:
+    DispSender( std::ostream & transport );
+
+    virtual
+    ~DispSender();
+
+    virtual
+    void sendShow() = 0;
+
+    virtual
+    void sendMsg() = 0;
+};
+
+/*!
+  \class DispSenderMonitor
+  \brief Base class for the visual protocol for monitors.
+*/
+class DispSenderMonitor
+    : public DispSender {
+public:
+
+    class Params {
+    public:
+        std::ostream & M_transport;
+        const Monitor & M_self;
+        const SerializerMonitor & M_serializer;
+        const Stadium & M_stadium;
+
+        Params( std::ostream & transport,
+                const Monitor & self,
+                const SerializerMonitor & serializer,
+                const Stadium & stadium )
+            : M_transport( transport )
+            , M_self( self )
+            , M_serializer( serializer )
+            , M_stadium( stadium )
+          { }
+    };
+
+
+private:
+    const SerializerPlayer & M_serializer;
+
+    /*:TODO: M_self needs to be replaced with a reference to a
+      Observer and Observer should have virtual functions for
+      stuff like velocity, stamina, etc */
+    const Monitor & M_self;
+    const Stadium & M_stadium;
+
+public:
+    typedef std::auto_ptr< DispSenderMonitor > Ptr;
+    typedef Ptr (*Creator)( const DispSenderMonitor::Params & );
+    typedef rcss::lib::Factory< Creator, int > Factory;
+
+    static
+    Factory & factory();
+
+    DispSenderMonitor( const Params & params );
+
+    ~DispSenderMonitor();
+
+protected:
+    const
+    SerializerMonitor &
+    serializer() const
+      {
+          return M_serializer;
+      }
+
+    const
+    Monitor & self() const
+      { return M_self; }
+
+    const
+    Stadium & stadium() const
+      {
+          return M_stadium;
+      }
+
+};
+
+
+/*!
+  \class ObserverMonitor
+  \brief Interface for players that receives display information.
+*/
+class ObserverMonitor
+    : protected BaseObserver< DispSenderMonitor >
+{
+public:
+
+    ObserverMonitor()
+      {}
+
+    ObserverMonitor( DispSenderMonitor & sender )
+        : BaseObserver< DispSenderMonitor >( sender )
+      {}
+
+    ObserverMonitor( std::auto_ptr< DispSenderMonitor > sender )
+        : BaseObserver< DispSenderMonitor >( sender )
+      {}
+
+    ~ObserverMonitor()
+      {}
+
+    void setDispSender( DispSenderMonitor & sender )
+      {
+          BaseObserver< DispSenderPlayer >::setSender( sender );
+      }
+
+    void setDispSender( std::auto_ptr< DispSenderMonitor > sender )
+      {
+          BaseObserver< DispSenderMonitor >::setSender( sender );
+      }
+
+    void sendShow()
+      {
+          BaseObserver< VisualSenderPlayer >::sender().sendShow();
+      }
+
+    void sendMsg()
+      {
+          BaseObserver< VisualSenderPlayer >::sender().sendMsg();
+      }
+
+};
+
+
+/*!
+  \class DispSenderMonitorV1
+  \brief class for the version 1 display protocol.
+*/
+class DispSenderMonitorV1
+    : public DispSenderMonitor {
+public:
+
+    DispSenderMonitorV1( const Params & params );
+
+    virtual
+    ~DispSenderMonitorV1();
+
+    virtual
+    void sendShow();
+
+    virtual
+    void sendMsg();
+
+};
+
+
+/*!
+  \class DispSenderMonitorV1
+  \brief class for the version 1 display protocol.
+*/
+class DispSenderMonitorV1
+    : public DispSenderMonitor {
+public:
+
+    DispSenderMonitorV1( const Params & params );
+
+    virtual
+    ~DispSenderMonitorV1();
+
+    virtual
+    void sendShow();
+
+    virtual
+    void sendMsg();
+
+};
+
+
+}
+
+#endif
