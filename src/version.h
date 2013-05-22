2ba4af988bf4587193b6c2876b5ba1057da4349e
akym <akym@1639d2a8-b03b-4162-8b0b-03f2cde17881>
2013-05-22 07:18:23 +0000

    2013-05-22  Hidehisa Akiyama  <akky@users.sourceforge.jp>
    	* NEWS:
    	* configure.ac:
    	- updated a minor version number. Official release 15.2.0.
    
    	* src/version.h:
    	- updated the copyright message.
    
    
    
    git-svn-id: file:///home/akiyama/sserver/svn/rcss/trunk/rcssserver@647 1639d2a8-b03b-4162-8b0b-03f2cde17881

--- src/version.h
+++ src/version.h
@@ -33,4 +33,4 @@
 
 
 char Copyright[] = "Copyright (C) 1995, 1996, 1997, 1998, 1999 Electrotechnical Laboratory.\n\
-2000 - 2012 RoboCup Soccer Simulator Maintenance Group.\n";
+2000 - RoboCup Soccer Simulator Maintenance Group.\n";
